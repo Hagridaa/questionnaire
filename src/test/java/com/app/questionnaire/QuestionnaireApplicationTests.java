04ea603a782c106990e6300b373a339d67c19526
hagridaa <annekristiinarasanen@gmail.com>
2019-11-12 20:07:17 +0200

    Unit test for user entity

--- src/test/java/com/app/questionnaire/QuestionnaireApplicationTests.java
+++ src/test/java/com/app/questionnaire/QuestionnaireApplicationTests.java
@@ -1,13 +1,25 @@
 package com.app.questionnaire;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.app.questionnaire.web.UserController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class QuestionnaireApplicationTests {
+	
+	@Autowired
+	private UserController userController;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads()  throws Exception {
+		assertThat(userController).isNotNull();
 	}
 
 }
---
+++ src/test/java/com/app/questionnaire/UserTest.java
@@ -0,0 +1,43 @@
+package com.app.questionnaire;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.app.questionnaire.domain.Questionnaire;
+import com.app.questionnaire.domain.User;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//annotation to use for regular
+//tests, as well as some specialized variants for testing parts of your application
+public class UserTest {
+
+	@Test
+	public void createNewUser() {	
+		//Long id, String username, String passwordHass, String role, String email
+		User user3 = new User(null, "user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "pii@gmail.com");
+		System.out.println("tulosta testi user3: " + user3.toString());
+		assertThat(user3.getUsername()).isEqualTo("user");
+		//assertThat(user3.getUsername().endsWith("r"));
+		//assertThat(user3.getUsername().isEmpty());
+	}
+	
+	@Test
+	public void newUser_shouldHaveQuestionnares() {
+		User user4 = new User(null, "user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "piippo@gmail.com");
+		Questionnaire questionnaire1 = new Questionnaire(null, "testQuestionnaire", "description ", user4);
+		Questionnaire quesitonnaire2 = new Questionnaire(null, "testQuestionnaire2", "description ", user4);
+		List<Questionnaire> questionnaires = List.of(questionnaire1, quesitonnaire2);
+		user4.setQuestionnares(questionnaires);
+		assertThat(user4.getQuestionnares()).isNotNull();
+	}
+}
+
