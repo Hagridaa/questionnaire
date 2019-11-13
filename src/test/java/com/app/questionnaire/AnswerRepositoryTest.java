0f2a39fc01036b33dc25d47063f788c97b92c448
hagridaa <annekristiinarasanen@gmail.com>
2019-11-13 21:25:43 +0200

    Added one more test to AnswerRepositoryTest

--- src/test/java/com/app/questionnaire/AnswerRepositoryTest.java
+++ src/test/java/com/app/questionnaire/AnswerRepositoryTest.java
@@ -54,5 +54,30 @@ public class AnswerRepositoryTest {
 		assertEquals("Minusta suklaa on hyvää", foundedAnswer1.get().getAnswerText());
 		
 	}
+	
+	@Test
+	public void createNewAnswer_userShouldBeCorrect() {
+		
+		User user7 = new User(null, "user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER","pm@gmail.com");
+		uRepository.save(user7);
+		
+		User user8 = new User(null, "user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER","pm@gmail.com");
+		uRepository.save(user8);
+		
+		Questionnaire questionnaire7 = new Questionnaire(null, "testQuestionnaire", "description ", user7);
+		Question question2 = new Question(null, "Onko suklaa hyvää?", new Questiontype(null,"text"),questionnaire7);
+		qRepository.save(question2);
+		System.out.println("tulosta kysymys2 "+ question2);
+		
+		Answer answer = new Answer(null,"Minusta suklaa on hyvää",question2);
+		System.out.println("Vastaus1 on: " + answer);
+		
+		aRepository.save(answer);
+		
+		Optional<Answer> foundedAnswer1 = aRepository.findById(answer.getAnswerId());
+		System.out.println("Löydetty vastaus1 on: " + foundedAnswer1.toString());
+		
+		assertEquals(user7, foundedAnswer1.get().getQuestion().getQuestionnaire().getUser());
+	}
 
 }
