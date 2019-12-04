1b5527c4b56bc0a36c9948d4e617e07693093048
hagridaa <annekristiinarasanen@gmail.com>
2019-12-04 11:22:22 +0200

    added UserDetailServiceImpl + modif pom + added new finfby to urepo

--- src/main/java/com/app/questionnaire/domain/UserRepository.java
+++ src/main/java/com/app/questionnaire/domain/UserRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 
+
 public interface UserRepository extends CrudRepository<User,Long> {
 Optional<User> findByUsername(String username);
 //voidaan etsiä tietokannasta usernamella käyttäjiä
+User findByUsername2(String username);
 }
---
+++ src/main/java/com/app/questionnaire/web/UserDetailServiceImpl.java
@@ -0,0 +1,31 @@
+package com.app.questionnaire.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.app.questionnaire.domain.User;
+import com.app.questionnaire.domain.UserRepository;
+
+//this class is used by spring security to authenticate ans authorize user
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+	 private final UserRepository repository;
+	 
+@Autowired public UserDetailServiceImpl(UserRepository userRepository) {
+	    	this.repository = userRepository;
+
+}
+@Override
+public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+	User curruser = repository.findByUsername2(username);
+	UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHass(),
+			AuthorityUtils.createAuthorityList(curruser.getRole()));
+	return user;
+}
+}
+
